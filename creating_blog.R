460639753a20c66a4fb15364ee79eefbfb075d81
j-desilva <j.amy.desilva@gmail.com>
2024-03-09 10:33:37 +1100

    added a second post by duplicating folders in both site and posts folders. there needs to be an index file for it to work.

--- creating_blog.R
+++ creating_blog.R
@@ -1,3 +1,5 @@
 library("distill")
 
-distill::create_blog(dir = "data-viz_blog", title = "JD Data Viz")
+#distill::create_blog(dir = "data-viz_blog", title = "JD Data Viz")
+
+#distill::create_post("a second post")
